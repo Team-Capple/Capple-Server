4315ff1e5472cb5963bc417d758fd455e0c4ae45
tnals2384 <tnals2384@gmail.com>
2024-02-13 15:34:55 +0900

    project setting

---
+++ src/main/java/com/server/capple/CappleApplication.java
@@ -0,0 +1,13 @@
+package com.server.capple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CappleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CappleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/capple/CappleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.capple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CappleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
