1092cfe462955a0bb64bb6536b10417e178e5850
tnals2384 <tnals2384@gmail.com>
2024-02-23 11:57:36 +0900

    feat: Question 엔티티 생성

--- src/main/java/com/server/capple/domain/question/entity/Question.java
+++ src/main/java/com/server/capple/domain/question/entity/Question.java
@@ -1,13 +1,15 @@
-package com.server.capple.domain.answer.entity;
+package com.server.capple.domain.question.entity;
 
-import com.server.capple.domain.member.entity.Member;
-import com.server.capple.domain.question.entity.Question;
+import com.server.capple.domain.answer.entity.Answer;
 import com.server.capple.global.common.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.SQLRestriction;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Builder
@@ -16,19 +18,20 @@ import org.hibernate.annotations.SQLRestriction;
 @AllArgsConstructor
 @SQLRestriction("deleted_at is null")
 @DynamicInsert
-public class Answer extends BaseEntity {
+public class Question extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="member_id", nullable = false)
-    private Member member;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="question_id", nullable = false)
-    private Question question;
+    @Column(nullable = false)
+    private String content;
 
     @ColumnDefault("0")
-    private int heartCount;
+    private String heartCount;
+
+    @OneToMany(mappedBy = "question", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @Builder.Default
+    private List<Answer> answers = new ArrayList<>();
+
+
 }
