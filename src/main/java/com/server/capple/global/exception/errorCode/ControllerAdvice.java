b31b4df7ce72fa8edd36f74f63861f5894588dd6
KyungsooLee <jmtkd9196@gmail.com>
2024-02-25 17:04:38 +0900

    feat: ControllerAdvice 구현

---
+++ src/main/java/com/server/capple/global/exception/errorCode/ControllerAdvice.java
@@ -0,0 +1,17 @@
+package com.server.capple.global.exception.errorCode;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerAdvice {
+
+    /**
+     * BaseException
+     */
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<ExceptionResponse> baseExceptionHandler(BaseException e) {
+        return new ResponseEntity<>(new ExceptionResponse(e.getErrorCode(), e.getMessage()), e.getStatus());
+    }
+}
