67b53d1b03385eb5483503567d4aaa00ea55f29a
zer0silver <duddms0_0@naver.com>
2024-02-29 21:36:20 +0900

    feat: #12 S3 이미지 업로드 유틸 추가

---
+++ src/main/java/com/server/capple/global/utils/S3ImageComponent.java
@@ -0,0 +1,59 @@
+package com.server.capple.global.utils;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class S3ImageComponent {
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    /**
+     * 이미지 업로드
+     * @param multipartFile 업로드 할 이미지 파일
+     * @return 업로드된 파일의 접근 URL
+     */
+    public String uploadImage(MultipartFile multipartFile) throws IOException {
+        String fileName = createFileName(multipartFile.getOriginalFilename());
+
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(multipartFile.getSize());
+        metadata.setContentType(multipartFile.getContentType());
+
+        amazonS3.putObject(bucket, fileName, multipartFile.getInputStream(), metadata);
+        return amazonS3.getUrl(bucket, fileName).toString();
+    }
+
+    /**
+     * 이미지 파일명 생성
+     * @param originalFileName 파일의 이름
+     * @return 작명된 파일 이름
+     */
+    public String createFileName(String originalFileName) {
+        return getOriginalFileName(originalFileName) + UUID.randomUUID().toString().replaceAll("-","") + getFileExtension(originalFileName);
+    }
+
+    private String getFileExtension(String fileName) {
+        return fileName.substring(fileName.lastIndexOf("."));
+    }
+    private String getOriginalFileName(String fileName) { return fileName.substring(0, fileName.lastIndexOf(".")); }
+
+    /**
+     * 버킷에서 이미지 삭제
+     * @param fileUrl
+     */
+    public void deleteImage(String fileUrl) {
+        amazonS3.deleteObject(new DeleteObjectRequest(bucket, fileUrl.split("/", 4)[3]));
+    }
+}
