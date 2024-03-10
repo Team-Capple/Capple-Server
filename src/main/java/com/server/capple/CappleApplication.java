cfe044eb1b2def1e50d41327c676d491d915be64
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2024-03-10 18:23:25 +0900

    feat: #3 properties 어노테이션 추가

--- src/main/java/com/server/capple/CappleApplication.java
+++ src/main/java/com/server/capple/CappleApplication.java
@@ -3,12 +3,14 @@ package com.server.capple;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableJpaAuditing
 @EnableFeignClients
+@EnableConfigurationProperties
 public class CappleApplication {
 
 	public static void main(String[] args) {
