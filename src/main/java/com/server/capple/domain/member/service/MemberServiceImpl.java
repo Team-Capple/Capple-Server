483411d78f859321539174c4ee234f1d138cbdca
zer0silver <duddms0_0@naver.com>
2024-03-12 09:12:09 +0900

    refactor: #24 디폴트 이미지 처리 방식 수정

--- src/main/java/com/server/capple/domain/member/service/MemberServiceImpl.java
+++ src/main/java/com/server/capple/domain/member/service/MemberServiceImpl.java
@@ -28,10 +28,6 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public class MemberServiceImpl implements MemberService {
 
-    // 기본 프로필 이미지
-    // todo: 기본 프로필 디자인 작업 완료 시 변경
-    private static final String DEFAULT_PROFILE_IMAGE = "https://capple-bucket.s3.ap-northeast-2.amazonaws.com/capple_default_image.png";
-
     private final MemberRepository memberRepository;
     private final MemberMapper memberMapper;
     private final S3ImageComponent s3ImageComponent;
@@ -59,12 +55,11 @@ public class MemberServiceImpl implements MemberService {
     public MemberResponse.EditMemberInfo editMemberInfo(Long memberId, MemberRequest.EditMemberInfo request) {
         Member member = findMember(memberId);
 
-        // 이전 이미지 버킷에서 삭제
-        s3ImageComponent.deleteImage(member.getProfileImage());
-
         // 1. 이미지 업데이트
-        String imageUrl = Optional.ofNullable(request.getProfileImage()).orElse(DEFAULT_PROFILE_IMAGE);
-        member.updateProfileImage(imageUrl);
+        String profileImage = request.getProfileImage();
+        if (profileImage != null) {
+            member.updateProfileImage(profileImage);
+        }
 
         // 2. 닉네임 업데이트
         if (memberRepository.countMemberByNickname(request.getNickname(), memberId) > 0) throw new RestApiException(MemberErrorCode.EXIST_MEMBER_NICKNAME);
