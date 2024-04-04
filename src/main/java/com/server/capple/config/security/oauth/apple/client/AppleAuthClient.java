83552bc583178f876ea4c5277f1f482bdc5ba890
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2024-04-05 08:09:19 +0900

    Feat: #83 로그인 시 클라이언트 에러 핸들링 및 반환 정의

--- src/main/java/com/server/capple/config/security/oauth/apple/client/AppleAuthClient.java
+++ src/main/java/com/server/capple/config/security/oauth/apple/client/AppleAuthClient.java
@@ -3,12 +3,13 @@ package com.server.capple.config.security.oauth.apple.client;
 import com.server.capple.config.feign.AppleFeignClientConfig;
 import com.server.capple.config.security.oauth.apple.dto.request.IdTokenRequest;
 import com.server.capple.config.security.oauth.apple.dto.response.AppleSocialTokenResponse;
+import feign.FeignException;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "appleAuthClient", url = "https://appleid.apple.com/auth", configuration = AppleFeignClientConfig.class)
 public interface AppleAuthClient {
     @PostMapping(value = "/token", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    AppleSocialTokenResponse generateAndValidateToken(IdTokenRequest request);
+    AppleSocialTokenResponse generateAndValidateToken(IdTokenRequest request) throws FeignException;
 }
