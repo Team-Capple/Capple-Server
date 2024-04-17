818a0f57d01180965cc8dce0a291a17cb3897f82
tnals2384 <tnals2384@gmail.com>
2024-04-17 15:54:56 +0900

    fix: #99 질문 목록 반환 시 live인 질문은 redis에서, old 질문은 rdb에서 tag 반환

--- src/main/java/com/server/capple/domain/question/scheduler/QuestionScheduler.java
+++ src/main/java/com/server/capple/domain/question/scheduler/QuestionScheduler.java
@@ -15,17 +15,15 @@ public class QuestionScheduler {
     //초 분 시 일 월 요일
     @Scheduled(cron = "0 0 7,18 * * *") //매일 오전 7시에, 오후 6시에
     public void setLiveQuestion() {
-
         adminQuestionService.setLiveQuestion();
         log.info("live question이 등록되었습니다.");
     }
 
     @Scheduled(cron = "0 0 1,14 * * *") //매일 오전 1시에, 오후 14시에
     public void closeLiveQuestion() {
-        //question을 닫고, rdb에 popular tags 저장
+        //question을 닫고, rgb popular tags 저장
         adminQuestionService.savePopularTags(adminQuestionService.closeLiveQuestion().getQuestionId());
 
         log.info("live question이 닫혔습니다.");
-
     }
 }
--- src/main/java/com/server/capple/domain/question/service/QuestionServiceImpl.java
+++ src/main/java/com/server/capple/domain/question/service/QuestionServiceImpl.java
@@ -5,21 +5,25 @@ import com.server.capple.domain.member.entity.Member;
 import com.server.capple.domain.question.dto.response.QuestionResponse.QuestionInfos;
 import com.server.capple.domain.question.dto.response.QuestionResponse.QuestionSummary;
 import com.server.capple.domain.question.entity.Question;
+import com.server.capple.domain.question.entity.QuestionStatus;
 import com.server.capple.domain.question.mapper.QuestionMapper;
 import com.server.capple.domain.question.repository.QuestionRepository;
 import com.server.capple.domain.tag.repository.TagRedisRepository;
+import com.server.capple.domain.tag.service.TagService;
 import com.server.capple.global.exception.RestApiException;
 import com.server.capple.global.exception.errorCode.QuestionErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.server.capple.domain.question.entity.QuestionStatus.*;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class QuestionServiceImpl implements QuestionService {
     private final QuestionRepository questionRepository;
-    private final TagRedisRepository tagRedisRepository;
+    private final TagService tagService;
     private final AnswerRepository answerRepository;
     private final QuestionMapper questionMapper;
 
@@ -44,9 +48,13 @@ public class QuestionServiceImpl implements QuestionService {
         return questionMapper.toQuestionInfos(questionRepository.findAllByQuestionStatusIsLiveAndOldByOrderByCreatedAtDesc().orElseThrow(()
                         -> new RestApiException(QuestionErrorCode.QUESTION_NOT_FOUND))
                 .stream()
-                .map(question -> questionMapper.toQuestionInfo(question,
-                        String.join(" ", tagRedisRepository.getTagsByQuestion(question.getId(), 7)),
-                        answerRepository.existsByQuestionAndMember(question, member)))
-                .toList());
+                .map(question -> {
+                    String tags = question.getQuestionStatus().equals(LIVE) ?
+                            String.join(" ", tagService.getTagsByQuestion(question.getId(),3).getTags()) :
+                            question.getPopularTags().trim();
+
+                    return questionMapper.toQuestionInfo(question, tags,
+                        answerRepository.existsByQuestionAndMember(question, member));
+                }).toList());
     }
 }
