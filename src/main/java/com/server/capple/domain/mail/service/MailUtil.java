052ff1ceffb768d5b5d8a988561c4c9e993ba694
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2024-09-23 01:45:26 +0900

    feat: #176 인증 메일 발송 메서드 비동기 처리

--- src/main/java/com/server/capple/domain/mail/service/MailUtil.java
+++ src/main/java/com/server/capple/domain/mail/service/MailUtil.java
@@ -1,10 +1,12 @@
 package com.server.capple.domain.mail.service;
 
+import java.util.concurrent.CompletableFuture;
+
 public interface MailUtil {
     public static Boolean emailAddressFormVerification(String emailAddress) {
         String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
         return emailAddress.matches(emailRegex);
     }
 
-    String sendMailAddressCertificationMail(String receiver, Boolean isWhiteList);
+    CompletableFuture<String> sendMailAddressCertificationMail(String receiver, Boolean isWhiteList);
 }
