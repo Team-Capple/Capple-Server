48a934ce599658b302231d42a73f5c0a2588f92b
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2024-09-25 00:52:30 +0900

    feat: #178 원격 알림 양식 변경

--- src/main/java/com/server/capple/domain/notifiaction/entity/NotificationType.java
+++ src/main/java/com/server/capple/domain/notifiaction/entity/NotificationType.java
@@ -6,10 +6,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum NotificationType {
-    BOARD_HEART("누군가 내 게시글을 좋아했어요"),
+    BOARD_HEART("누군가 내 게시글에 좋아요를 눌렀어요"),
     BOARD_COMMENT("누군가 내 게시글에 댓글을 달았어요"),
-    BOARD_COMMENT_DUPLICATE("누군가 같은 게시글에 댓글을 달았어요"),
-    BOARD_COMMENT_HEART("누군가 내 댓글을 좋아했어요"),
+    BOARD_COMMENT_DUPLICATE("누군가 댓글을 달았어요"),
+    BOARD_COMMENT_HEART("누군가 내 댓글에 좋아요를 눌렀어요"),
     TODAY_QUESTION_PUBLISHED("오늘의 질문 준비 완료!"),
     TODAY_QUESTION_CLOSED("오늘의 질문 답변 마감!"),
     ;
