0387ae0c8acdbb8c65ad50bff18e623b0171b353
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2024-09-28 21:54:29 +0900

    feat: #188 질문 총 개수 캐싱 로직 구현

--- src/main/java/com/server/capple/domain/question/service/AdminQuestionServiceImpl.java
+++ src/main/java/com/server/capple/domain/question/service/AdminQuestionServiceImpl.java
@@ -9,8 +9,10 @@ import com.server.capple.domain.question.repository.AdminQuestionRepository;
 import com.server.capple.global.exception.RestApiException;
 import com.server.capple.global.exception.errorCode.QuestionErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Service
 @RequiredArgsConstructor
@@ -19,11 +21,13 @@ public class AdminQuestionServiceImpl implements AdminQuestionService {
     private final AdminQuestionRepository adminQuestionRepository;
     private final QuestionService questionService;
     private final QuestionMapper questionMapper;
+    private final QuestionCountService questionCountService;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
     @Override
     public QuestionId createQuestion(QuestionCreate request) {
         Question question = adminQuestionRepository.save(questionMapper.toQuestion(request));
-
+        applicationEventPublisher.publishEvent(new QuestionStatusChangedEvent());
         return new QuestionId(question.getId());
     }
 
@@ -33,7 +37,7 @@ public class AdminQuestionServiceImpl implements AdminQuestionService {
         Question question = questionService.findQuestion(questionId);
 
         question.delete();
-
+        applicationEventPublisher.publishEvent(new QuestionStatusChangedEvent());
         return new QuestionId(question.getId());
 
     }
@@ -41,10 +45,10 @@ public class AdminQuestionServiceImpl implements AdminQuestionService {
     @Transactional
     public Question setLiveQuestion() {
         Question newQuestion = adminQuestionRepository.findFirstByQuestionStatusOrderByIdAsc(QuestionStatus.PENDING)
-                .orElseThrow(() -> new RestApiException(QuestionErrorCode.QUESTION_PENDING_NOT_FOUND));
+            .orElseThrow(() -> new RestApiException(QuestionErrorCode.QUESTION_PENDING_NOT_FOUND));
 
         newQuestion.setQuestionStatus(QuestionStatus.LIVE);
-
+        applicationEventPublisher.publishEvent(new QuestionStatusChangedEvent());
         return newQuestion;
 
     }
@@ -52,9 +56,17 @@ public class AdminQuestionServiceImpl implements AdminQuestionService {
     @Transactional
     public Question closeLiveQuestion() {
         Question question = adminQuestionRepository.findFirstByQuestionStatusOrderByIdAsc(QuestionStatus.LIVE)
-                .orElseThrow(() -> new RestApiException(QuestionErrorCode.QUESTION_LIVE_NOT_FOUND));
+            .orElseThrow(() -> new RestApiException(QuestionErrorCode.QUESTION_LIVE_NOT_FOUND));
         question.setQuestionStatus(QuestionStatus.OLD);
-
+        applicationEventPublisher.publishEvent(new QuestionStatusChangedEvent());
         return question;
+    }
+
+    static class QuestionStatusChangedEvent {
+    }
+
+    @TransactionalEventListener(QuestionStatusChangedEvent.class)
+    public void handleBoardCreatedEvent() {
+        questionCountService.updateLiveOrOldQuestionCount();
     }
 }
