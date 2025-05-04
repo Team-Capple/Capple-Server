5c22f746a8fd36c747b368ae7478fa3567584bf4
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2025-05-04 23:47:01 +0900

    feat #265 : SpEL key 기반 분산락 트랜잭션 어노테이션 구현

---
+++ src/main/java/com/server/capple/global/utils/distributedLock/AopForTransaction.java
@@ -0,0 +1,14 @@
+package com.server.capple.global.utils.distributedLock;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+class AopForTransaction {
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public Object proceed(ProceedingJoinPoint joinPoint) throws Throwable {
+        return joinPoint.proceed();
+    }
+}
