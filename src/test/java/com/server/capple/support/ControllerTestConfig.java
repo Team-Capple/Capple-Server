1a685f46271757a31045bf16c8e74b3d7b23b0c2
KyungsooLee <jmtkd9196@gmail.com>
2025-05-29 04:23:24 +0900

    feat: #283 AnswerCommentHeart 관련 테스트 로직 수정

--- src/test/java/com/server/capple/support/ControllerTestConfig.java
+++ src/test/java/com/server/capple/support/ControllerTestConfig.java
@@ -164,15 +164,34 @@ public abstract class ControllerTestConfig {
             .build();
     }
 
-    protected AnswerCommentInfos getAnswerCommentInfos() {
-        List<AnswerCommentInfo> answerCommentInfos = List.of(AnswerCommentInfo.builder()
-            .answerCommentId(1L)
-            .writerId(member.getId())
-            .content("댓글 1")
-            .createdAt(LocalDateTime.of(2022, 11, 1, 12, 02))
-            .heartCount(3)
-            .build());
+//    protected AnswerCommentInfos getAnswerCommentInfos() {
+//        List<AnswerCommentInfo> answerCommentInfos = List.of(AnswerCommentInfo.builder()
+//            .answerCommentId(1L)
+//            .writerId(member.getId())
+//            .content("댓글 1")
+//            .createdAt(LocalDateTime.of(2022, 11, 1, 12, 02))
+//            .heartCount(3)
+//            .build());
+//
+//        return new AnswerCommentInfos(answerCommentInfos);
+//    }
 
-        return new AnswerCommentInfos(answerCommentInfos);
+    protected SliceResponse<AnswerCommentInfo> getSliceAnswerCommentInfos() {
+        List<AnswerCommentInfo> answerCommentInfos = List.of(AnswerCommentInfo.builder()
+                .answerCommentId(1L)
+                .writerId(member.getId())
+                .content("댓글 1")
+                .createdAt(LocalDateTime.of(2022, 11, 1, 12, 2))
+                .heartCount(3)
+                .isLiked(true)
+                .isMine(true)
+                .build());
+
+        return SliceResponse.<AnswerCommentInfo>builder()
+                .size(10)
+                .content(answerCommentInfos)
+                .numberOfElements(1)
+                .hasNext(FALSE)
+                .build();
     }
 }
